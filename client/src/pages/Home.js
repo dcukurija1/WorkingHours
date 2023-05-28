b45c9712f37507193482aff5f554cd6db63397f0
dcukurija1 <dcukurija1@etf.unsa.ba>
2023-05-28 15:21:05 +0200

    ammend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,28 @@
 import React from 'react'
+import WorkingHoursTable from '../components/WorkingHoursTable';
 
-const Home = () => {
-  return (
-		<div>
-			<h1>Working hours</h1>
-			<label>Input date: </label>
-			<input type="date" />
-			<label>Input working hours: </label>
-
-			<label>From: </label>
-			<input type="time" />
-			<label>To: </label>
-			<input type="time" />
-			<label>Input short daily report</label>
-			<input type="text-area" />
+import '../index.css'
+import { Button } from '@mui/material';
+const Home = ({ user, hours }) => {
+	
+	const addNewEntry = () => {
+		console.log('Adding a new entry');
+	}
+	return (
+		<div className='home'>
+			<h1>{user.name}</h1>
+			<WorkingHoursTable hours={hours} />
+			<Button
+				color='primary'
+				variant='contained'
+				size='small'
+				onClick={addNewEntry}
+			>
+				add new entry +
+			</Button>
 		</div>
 	);
 }
+
 
 export default Home
